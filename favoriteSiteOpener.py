c80effa4fb23b361d60e19d4849ebde969996bd0
olaade97 <57572771+olaade97@users.noreply.github.com>
2020-08-15 17:51:56 -0500

    Add files via upload

---
+++ favoriteSiteOpener.py
@@ -0,0 +1,20 @@
+# mapIt.py - Launches a map in the browser using an address from the
+# command line or clipboard.
+#! python3
+import webbrowser, sys, pyperclip
+
+
+# Get address from command line.
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+    
+else: 
+    # Get address from clipboard.
+    website = pyperclip.paste()
+    webbrowser.open(website)
+    
+webbrowser.open('https://www.reddit.com/r/frugalmalefashion/')
+webbrowser.open('https://www.reddit.com/r/consoledeals/')
+webbrowser.open('https://www.reddit.com/r/SneakerDeals/')
+
+# TODO: Get address from clipboard.
